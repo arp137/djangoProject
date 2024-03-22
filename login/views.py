d63eb7fb9312f52200ddc7b3ece879b94254c094
Albert <albertricart2@gmail.com>
2024-03-23 00:27:54 +0100

    User authentication

--- login/views.py
+++ login/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 
 def homepage(request):
     return render(request, 'index.html')
 
 def my_login(request):
-    return render(request, 'login.html')
+
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+
+    context = {'loginform': form}
+
+    return render(request, 'login.html', context=context)
 
 def dashboard(request):
     return render(request, 'dashboard.html')
