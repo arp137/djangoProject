f6a030c769f005aebf808da5cce722ad9548a214
Albert <albertricart2@gmail.com>
2024-03-23 00:11:28 +0100

    User registration correct

--- login/urls.py
+++ login/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home'),
-    path('register', views.register, name='register'),
-    path('login', views.my_login, name='login'),
-    path('dashboard', views.dashboard, name='dashboard')
-
+    path('register/', views.register, name='register'),
+    path('login/', views.my_login, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard')
 ]
--- login/views.py
+++ login/views.py
@@ -1,32 +1,23 @@
 from django.shortcuts import render, redirect
-from . forms import CreateUserForm
+from .forms import CreateUserForm
 
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
 def homepage(request):
-    return render(request, 'login/index.html')
-
+    return render(request, 'index.html')
 
 def my_login(request):
-    return render(request, 'login/login.html')
-
+    return render(request, 'login.html')
 
 def dashboard(request):
-    return render(request, 'login/dashboard.html')
+    return render(request, 'dashboard.html')
 
 def register(request):
-
     form = CreateUserForm()
 
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("my-login")
+            return redirect("login")
 
-    context = {'registerform':form}
-
-    return render(request, 'login/register.html', context=context)
+    context = {'registerform': form}
+    return render(request, 'register.html', context=context)
