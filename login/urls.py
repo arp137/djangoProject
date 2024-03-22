e1cac087049fb2b2717a0f8a5690d15aefa9e470
Albert <albertricart2@gmail.com>
2024-03-22 21:40:57 +0100

    User registration

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.homepage, name='home'),
+    path('register', views.register, name='register'),
+    path('login', views.my_login, name='login'),
+    path('dashboard', views.dashboard, name='dashboard')
 
 ]
--- login/views.py
+++ login/views.py
@@ -1,9 +1,32 @@
+from django.shortcuts import render, redirect
+from . forms import CreateUserForm
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def homepage(request):
-    pass
+    return render(request, 'login/index.html')
 
-def regiseter(request):
-    pass
+
+def my_login(request):
+    return render(request, 'login/login.html')
+
+
+def dashboard(request):
+    return render(request, 'login/dashboard.html')
+
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("my-login")
+
+    context = {'registerform':form}
+
+    return render(request, 'login/register.html', context=context)
