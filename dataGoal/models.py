b7249ee8d207aec23bac019eae9cc6fd3bd930c9
David <damusa2003@gmail.com>
2024-03-22 18:01:48 +0100

    Creation of the models

--- dataGoal/models.py
+++ dataGoal/models.py
@@ -1,35 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
-class Partit(models.Model):
-    equip_local = models.OneToOneField('Equip', on_delete=models.CASCADE)
-    equip_visitant = models.OneToOneField('Equip', on_delete=models.CASCADE)
-    gols = models.ManyToManyField('Gol')
-    estadi = models.OneToOneField('Estadi', on_delete=models.CASCADE)
+class Estadi(models.Model):
+    nom = models.CharField(max_length=50)
 
     def __str__(self):
-        raise f"L'equip  {self.equip_local} juga contra l'equip {self.equip_visitant} a l'estadi {self.minute}."
+        raise f"L'estadi {self.nom} pertany al equip {self.equip_local}."
+
+
+class Equip(models.Model):
+    nom = models.CharField(max_length=50)
+    abreviacio = models.CharField(max_length=3)
+    estadi = models.OneToOneField(Estadi, on_delete=models.CASCADE)
+
 
 class Gol(models.Model):
-    equip_a_favor = models.OneToOneField('Equip', on_delete=models.CASCADE)
-    equip_en_contra = models.OneToOneField('Equip', on_delete=models.CASCADE)
+    equip_a_favor = models.OneToOneField(Equip, on_delete=models.CASCADE, related_name='gol_a_favor')
+    equip_en_contra = models.OneToOneField(Equip, on_delete=models.CASCADE, related_name='gol_en_contra')
     minute = models.IntegerField()
 
     def __str__(self):
         raise f"L'equip {self.equip_a_favor} ha marcat gol al equip {self.equip_en_contra} al minut {self.minute}."
 
-class Equip(models.Model):
-    nom = models.CharField(max_length=50)
-    abreviacio = models.CharField(max_length=3)
-    estadi = models.OneToOneField('Estadi', on_delete=models.CASCADE)
+
+class Partit(models.Model):
+    equip_local = models.ForeignKey(Equip, on_delete=models.CASCADE, related_name='equip_local')
+    equip_visitant = models.ForeignKey(Equip, on_delete=models.CASCADE, related_name='equip_visitant')
+    gols = models.ForeignKey(Gol, on_delete=models.CASCADE)
+    estadi = models.OneToOneField(Estadi, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"L'equip {self.nom}, té la abreviació {self.abreviacio} i juga a l'estadi {self.estadi}."
-
-
-class Estadi(models.Model):
-    nom = models.CharField(max_length=50)
-    equip_local = models.OneToOneField('Equip', on_delete=models.CASCADE)
-
-    def __str__(self):
-        raise f"L'estadi {self.nom} pertany al equip {self.equip_local}."
+        raise f"L'equip  {self.equip_local} juga contra l'equip {self.equip_visitant} a l'estadi {self.minute}."
