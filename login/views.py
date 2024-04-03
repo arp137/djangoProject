c58111da58b033cff08e01dcd486dbace7e452e2
arp137 <154314080+arp137@users.noreply.github.com>
2024-04-03 18:06:20 +0200

    login

--- login/views.py
+++ login/views.py
@@ -3,29 +3,32 @@ from .forms import CreateUserForm, LoginForm
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+
 
 
 def homepage(request):
     return render(request, 'index.html')
 
 
+# Vista de inicio de sesión personalizada
 def my_login(request):
     form = LoginForm()
 
     if request.method == 'POST':
-        form = LoginForm(request, data=request.POST)
+        form = LoginForm(request=request, data=request.POST)
         if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
 
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
-                auth.login(request, user)
+                auth_login(request, user)
                 return redirect("dashboard")
 
     context = {'loginform': form}
-
     return render(request, 'login.html', context=context)
 
 
